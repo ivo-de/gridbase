31fafbde3c9fef7af61d4d90f01cc867d4455897
lou <lou>
2021-12-01 17:11:04 +0000

    added getLOS, getLOSWithDirection (broken)

--- src/components/characterPanel.jsx
+++ src/components/characterPanel.jsx
@@ -1,36 +1,54 @@
-import styled from 'styled-components'
-import { convertX, convertY, isCharacter } from '../scripts/utils'
+import styled from "styled-components";
+import { convertX, convertY, isCharacter } from "../scripts/utils";
 
 const CharacterPanelWrapper = styled.div`
-    border: 1px solid black;
-    margin: 20px;
-    padding-left: 20px;
-`
+  border: 1px solid black;
+  margin: 20px;
+  padding-left: 20px;
+`;
 
 const Coordinates = styled.span`
-    font-family: consolas;
-`
+  font-family: consolas;
+`;
 
 export const CharacterPanel = (props) => {
-    const {game, updateKey} = props
-    const curChar = game.characters[game.selectedChar] 
+  const { game, updateKey } = props;
+  const curChar = game.characters[game.selectedChar];
 
-    if (curChar) {
-        const mapNames = (char) => char.name
-        const charsAdjacent = curChar.getAdjacentTiles(game).filter(isCharacter).map(mapNames).join(', ')
-        const charsInControl = curChar.getControlZoneTiles(game).filter(isCharacter).map(mapNames).join(', ')
-        return (
-            <CharacterPanelWrapper key={updateKey}>
-                <p>Currently selected character: {curChar.name}</p>
-                <p>Character location: <Coordinates>{convertX(curChar.x)}{convertY(curChar.y)}</Coordinates></p>
-                <p>Characters adjacent: {charsAdjacent}</p>
-                <p>Characters in control zone: {charsInControl}</p>
-            </CharacterPanelWrapper>
-        )
-    }
+  if (curChar) {
+    const mapNames = (char) => char.name;
+    const charsAdjacent = curChar
+      .getAdjacentTiles(game)
+      .filter(isCharacter)
+      .map(mapNames)
+      .join(", ");
+    const charsInControl = curChar
+      .getControlZoneTiles(game)
+      .filter(isCharacter)
+      .map(mapNames)
+      .join(", ");
+
+    const charsInLOS = curChar.getLOS(game).map(mapNames).join(", ");
+
     return (
-        <CharacterPanelWrapper key={updateKey}>
-            <p>No currently selected character</p>
-        </CharacterPanelWrapper>
-    )
-}
+      <CharacterPanelWrapper key={updateKey}>
+        <p>Currently selected character: {curChar.name}</p>
+        <p>
+          Character location:{" "}
+          <Coordinates>
+            {convertX(curChar.x)}
+            {convertY(curChar.y)}
+          </Coordinates>
+        </p>
+        <p>Characters adjacent: {charsAdjacent}</p>
+        <p>Characters in control zone: {charsInControl}</p>
+        <p>Characters in line of sight: {charsInLOS}</p>
+      </CharacterPanelWrapper>
+    );
+  }
+  return (
+    <CharacterPanelWrapper key={updateKey}>
+      <p>No currently selected character</p>
+    </CharacterPanelWrapper>
+  );
+};
--- src/scripts/character.js
+++ src/scripts/character.js
@@ -1,101 +1,147 @@
-import { isCharacter } from "./utils"
+import { isCharacter } from "./utils";
 
-const directions = ['left', 'up', 'right', 'down']
+const directions = ["left", "up", "right", "down"];
 
 export class Character {
-    constructor(name, x, y, color = 'red') {
-        this.name = name
-        this.x = x
-        this.y = y
-        this.color = color
-        this.selected = false
-        this.direction = 'down'
-        this.stats = {
-            vitality: 1,
-            movement: 1,
-            strength: 1,
-        }
-        this.currentStats = {
-            vitality: 1,
-            movement: 1,
-            strength: 1,
-        }
-    }
+  constructor(name, x, y, color = "red") {
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.selected = false;
+    this.direction = "down";
+    this.stats = {
+      vitality: 1,
+      movement: 1,
+      strength: 1,
+    };
+    this.currentStats = {
+      vitality: 1,
+      movement: 1,
+      strength: 1,
+    };
+  }
 
-    getAdjacentTiles(game) {
-        return game.getCellsAround(this.name)
-    }
+  getAdjacentTiles(game) {
+    return game.getCellsAround(this.name);
+  }
 
-    getControlZoneTiles(game) {
-        const cardinals = game.getCellsAroundDirs(this.name)
-        switch(this.direction) {
-            case 'up': 
-                return [cardinals.NW, cardinals.N, cardinals.NE]
-            case 'left':
-                return [cardinals.NW, cardinals.W, cardinals.SW]
-            case 'right':
-                return [cardinals.NE, cardinals.E, cardinals.SE]
-            default:
-                return [cardinals.SW, cardinals.S, cardinals.SE]
-        }
+  getControlZoneTiles(game) {
+    const cardinals = game.getCellsAroundDirs(this.name);
+    switch (this.direction) {
+      case "up":
+        return [cardinals.NW, cardinals.N, cardinals.NE];
+      case "left":
+        return [cardinals.NW, cardinals.W, cardinals.SW];
+      case "right":
+        return [cardinals.NE, cardinals.E, cardinals.SE];
+      default:
+        return [cardinals.SW, cardinals.S, cardinals.SE];
     }
+  }
 
-    turn(direction, game){
-        const curIdx = directions.indexOf(this.direction)
-        let nextIdx = 0
-        if (direction === 'left') {
-            nextIdx = curIdx + 1 === directions.length ? 0 : curIdx + 1
+  turn(direction, game) {
+    const curIdx = directions.indexOf(this.direction);
+    let nextIdx = 0;
+    if (direction === "left") {
+      nextIdx = curIdx + 1 === directions.length ? 0 : curIdx + 1;
+    } else {
+      nextIdx = curIdx - 1 < 0 ? directions.length - 1 : curIdx - 1;
+    }
+    game.turnCharacter(this.name, directions[nextIdx]);
+  }
+
+  move(cardinal, game) {
+    const moveTo = (x, y) => {
+      if (isCharacter(game.gridAt(x, y))) {
+        // fail to move if there's already a character there
+        return;
+      }
+      game.moveCharacter(this.name, x, y);
+    };
+    switch (cardinal) {
+      case "NW":
+        moveTo(this.x - 1, this.y - 1);
+        break;
+      case "N":
+        moveTo(this.x, this.y - 1);
+        break;
+      case "NE":
+        moveTo(this.x + 1, this.y - 1);
+        break;
+      case "E":
+        moveTo(this.x + 1, this.y);
+        break;
+      case "W":
+        moveTo(this.x - 1, this.y);
+        break;
+      case "SW":
+        moveTo(this.x - 1, this.y + 1);
+        break;
+      case "S":
+        moveTo(this.x, this.y + 1);
+        break;
+      case "SE":
+        moveTo(this.x + 1, this.y + 1);
+        break;
+      default:
+        break;
+    }
+  }
+
+  getLOS(game, callBackCheck) {
+    const yPos = this.y;
+    console.log("Y", yPos);
+    const charsInLOS = [];
+    for (var column in game.grid) {
+      for (var row in game.grid[column]) {
+        if (callBackCheck(row, yPos)) {
+          console.log("not in line of sight. column:", column, "cell:", row);
         } else {
-            nextIdx = curIdx - 1 < 0 ? directions.length -1 : curIdx - 1
+          if (game.grid[column][row] === null) {
+            console.log(
+              "in line of sight, empty. column:",
+              column,
+              "cell:",
+              row
+            );
+            console.log("trying to log the cell, ", game.grid[column][row]);
+          } else {
+            console.log(
+              "in line of sight, not empty. column:",
+              column,
+              "cell:",
+              row
+            );
+            charsInLOS.push(game.grid[column][row]);
+            break;
+          }
         }
-        game.turnCharacter(this.name, directions[nextIdx])
+      }
     }
+    return charsInLOS;
+  }
 
-    move(cardinal, game) {
-        const moveTo = (x, y) => {
-            if ( isCharacter(game.gridAt(x, y)) ) {
-                // fail to move if there's already a character there
-                return
-            }
-            game.moveCharacter(this.name, x, y)
-        }
-        switch(cardinal) {
-            case 'NW':
-                moveTo(this.x - 1, this.y - 1)
-            break
-            case 'N':
-                moveTo(this.x, this.y - 1)
-            break
-            case 'NE':
-                moveTo(this.x + 1, this.y - 1)
-            break
-            case 'E': 
-                moveTo(this.x + 1, this.y)
-            break
-            case 'W': 
-                moveTo(this.x - 1, this.y)
-            break
-            case 'SW':
-                moveTo(this.x - 1, this.y + 1)
-            break
-            case 'S':
-                moveTo(this.x, this.y + 1)
-            break
-            case 'SE':
-                moveTo(this.x + 1, this.y + 1)
-            break
-            default:
-            break
-        }
+  checkForLOSBelow(row, yPos) {
+    return row <= yPos;
+  }
+
+  getLOSWithDirection(game, row, yPos) {
+    switch (this.direction) {
+      case "up":
+        console.log("ok");
+      default:
+        return this.getLOS(game, this.checkForLOSBelow(row, yPos));
     }
+  }
 }
 
 export const generateCharacters = () => {
-    const John = new Character('John', 3, 1, 'cyan')
-    const Kevin = new Character('Kevin', 4, 1, 'cornflowerblue')
-    const Marcus = new Character('Marcus', 4, 8, 'red')
-    const Nick = new Character('Nick', 3, 8, 'maroon')
-    Nick.direction = 'up'
-    Marcus.direction = 'up'
-    return [John, Kevin, Marcus, Nick]
-}
+  const John = new Character("John", 3, 1, "cyan");
+  const Kevin = new Character("Kevin", 4, 1, "cornflowerblue");
+  const Marcus = new Character("Marcus", 4, 8, "red");
+  const Nick = new Character("Nick", 3, 8, "maroon");
+  Nick.direction = "up";
+  Marcus.direction = "up";
+  return [John, Kevin, Marcus, Nick];
+};
